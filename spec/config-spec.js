d02a365a31166faf3ca7e4a6f923ed3887b98ec1
Steven Soloff <steven.soloff@gmail.com>
2016-11-26 12:02:16 -0500

    Define skeletal configuration

---
+++ spec/config-spec.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016 Steven Soloff
+ *
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the MIT License (https://opensource.org/licenses/MIT).
+ * This software comes with ABSOLUTELY NO WARRANTY.
+ */
+
+'use strict';
+
+const config = require('../');
+
+describe('The configuration', () => {
+  it('should contain the required properties', () => {
+    expect(config).toBeNonEmptyObject();
+    expect(config.rules).toBeNonEmptyObject();
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016 Steven Soloff
+ *
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the MIT License (https://opensource.org/licenses/MIT).
+ * This software comes with ABSOLUTELY NO WARRANTY.
+ */
+
+'use strict';
+
+module.exports = require('./eslintrc.json');
