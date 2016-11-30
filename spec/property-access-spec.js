00a86aca5c29a9fd619742898c93a3f16863024b
Steven Soloff <steven.soloff@gmail.com>
2016-11-29 21:41:34 -0500

    Allow property access using square-bracket notation
    I can't find any restriction in the Crockford style guide regarding
    only using dot notation for property access.  However, the JSCS
    Crockford style config explicitly requires dot notation.  I'll revisit
    in the future if it turns out this is required.

---
+++ spec/property-access-spec.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016 Steven Soloff
+ *
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the MIT License (https://opensource.org/licenses/MIT).
+ * This software comes with ABSOLUTELY NO WARRANTY.
+ */
+
+'use strict'
+
+const linting = require('./support/test-util').linting
+const source = require('./support/test-util').source
+
+describe('Linting property access', () => {
+  it('should not report a violation when using square-bracket notation', () => {
+    const text = source([
+      'var foo = {};',
+      'foo["bar"];'
+    ])
+    expect(linting(text)).toNotReportViolation()
+  })
+
+  it('should not report a violation when using dot notation', () => {
+    const text = source([
+      'var foo = {};',
+      'foo.bar;'
+    ])
+    expect(linting(text)).toNotReportViolation()
+  })
+})
