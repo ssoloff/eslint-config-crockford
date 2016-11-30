92fb0fea29bd8187256f8699f7422c8f3344cf2f
Steven Soloff <steven.soloff@gmail.com>
2016-11-29 23:12:52 -0500

    Allow multiple var declarations per scope
    The Crockford style guide explicitly uses multiple var declarations as
    an example.  However, the JSCS Crockford style config explicitly
    disallows multiple var declarations.  I'll revisit in the future if it
    turns out this is required.

--- spec/variable-declarations-spec.js
+++ spec/variable-declarations-spec.js
@@ -12,7 +12,7 @@ const linting = require('./support/test-util').linting
 const source = require('./support/test-util').source
 
 describe('Linting variable declarations', () => {
-  it('should report a violation when var declarations are not first in function scope', () => {
+  it('should report a violation when var declarations are not first in scope', () => {
     const text = source([
       'while (1) {',
       '    var foo = 1;',
@@ -22,11 +22,22 @@ describe('Linting variable declarations', () => {
     expect(linting(text)).toReportViolationForRule('vars-on-top')
   })
 
-  it('should not report a violation when var declarations are first in function scope', () => {
+  it('should not report a violation when var declarations are first in scope', () => {
     const text = source([
       'var foo = 1;',
       'while (1) {',
       '    foo;',
+      '}'
+    ])
+    expect(linting(text)).toNotReportViolation()
+  })
+
+  it('should not report a violation when multiple var declarations are used in same scope', () => {
+    const text = source([
+      'var foo = 1;',
+      'var bar = 2;',
+      'while (foo) {',
+      '    bar;',
       '}'
     ])
     expect(linting(text)).toNotReportViolation()
