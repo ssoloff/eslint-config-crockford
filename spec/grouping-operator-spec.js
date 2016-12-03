b1851ac16dfe7b71a0073248bd2d3c674d164cd5
Steven Soloff <steven.soloff@gmail.com>
2016-12-02 22:40:10 -0500

    Add space-in-parens rule

---
+++ spec/grouping-operator-spec.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016 Steven Soloff
+ *
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the MIT License (https://opensource.org/licenses/MIT).
+ * This software comes with ABSOLUTELY NO WARRANTY.
+ */
+
+'use strict'
+
+const linting = require('./support/test-util').linting
+const source = require('./support/test-util').source
+
+describe('Linting grouping operator', () => {
+  it('should report a violation when a space appears after the opening parenthesis', () => {
+    const text = source(['( 1 + 2);'])
+    expect(linting(text)).toReportViolationForRule('space-in-parens')
+  })
+
+  it('should report a violation when a space appears before the closing parenthesis', () => {
+    const text = source(['(1 + 2 );'])
+    expect(linting(text)).toReportViolationForRule('space-in-parens')
+  })
+
+  it(
+    'should not report a violation when no space appears after the opening parenthesis or before the closing ' +
+        'parenthesis',
+    () => {
+      const text = source(['(1 + 2);'])
+      expect(linting(text)).toNotReportViolation()
+    }
+  )
+})
