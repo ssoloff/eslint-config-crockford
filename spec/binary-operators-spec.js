4fad3f1d0a46a71c3226ecae50ee877bc5c5d9d9
Steven Soloff <steven.soloff@gmail.com>
2016-12-06 22:24:35 -0500

    Add no-sequences rule

--- spec/binary-operators-spec.js
+++ spec/binary-operators-spec.js
@@ -23,13 +23,28 @@ describe('Linting binary operators', () => {
   })
 
   it('should report a violation when a space appears before the sequence operator', () => {
-    const text = source(['1 , 2;'])
+    const text = source([
+      'var i, j;',
+      'for (i = 0 , j = 0; i < 10; ++i, ++j) {',
+      '    1;',
+      '}'
+    ])
     expect(linting(text)).toReportViolationForRule('comma-spacing')
   })
 
   it('should report a violation when a space does not appear after the sequence operator', () => {
-    const text = source(['1,2;'])
+    const text = source([
+      'var i, j;',
+      'for (i = 0,j = 0; i < 10; ++i, ++j) {',
+      '    1;',
+      '}'
+    ])
     expect(linting(text)).toReportViolationForRule('comma-spacing')
+  })
+
+  it('should report a violation when the sequence operator is used outside of a for statement', () => {
+    const text = source(['1, 2;'])
+    expect(linting(text)).toReportViolationForRule('no-sequences')
   })
 
   it('should report a violation when the coercive equality operator is used', () => {
