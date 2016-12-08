a66bbba92ee04fa844daba2fe35b2369a8627719
Steven Soloff <steven.soloff@gmail.com>
2016-12-07 22:16:31 -0500

    Add no-eval rule

---
+++ spec/built-in-objects-spec.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016 Steven Soloff
+ *
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the MIT License (https://opensource.org/licenses/MIT).
+ * This software comes with ABSOLUTELY NO WARRANTY.
+ */
+
+'use strict'
+
+const linting = require('./support/test-util').linting
+const source = require('./support/test-util').source
+
+describe('Linting built-in objects', () => {
+  it('should report a violation when the eval() function is used', () => {
+    const text = source(['eval("1 + 1");'])
+    expect(linting(text)).toReportViolationForRule('no-eval')
+  })
+})
